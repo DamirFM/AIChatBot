fbede64098db4008483fcc4d60f53c0a46fea936
Damir Yakupov <yakupovdamir93@gmail.com>
2024-11-10 22:28:22 -0500

    last updates

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import AboutComponent from "@/app/components/about/page";
+
+
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col space-y-8">
+      <AboutComponent />
+    </div>
+
+
+  );
+}
